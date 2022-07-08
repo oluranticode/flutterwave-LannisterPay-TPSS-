942493de2b6f2a21dfc0973e29f313eb80a9b97c
youngtemmy <alukotemitope49@gmail.com>
2022-07-08 14:59:40 +0100

    update commit

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
-const connectDB = require('./db/connect')
+// const connectDB = require('./db/connect')
 const app = express();
 const router = require('./routes/payment');
 
-// error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
+
 
 // routes
 app.use('/split-payments/compute', router);
@@ -19,14 +16,12 @@ app.get('/', (req, res) => {
   res.send('LannisterPay(TPSS)');
 });
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
+    // await connectDB(process.env.MONGO_URL)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
